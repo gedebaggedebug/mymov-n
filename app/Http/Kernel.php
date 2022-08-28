423c2e7d3086a4a1bf2127cbb97d3455fe4e9b24
gedebaggedebug <Aviv.tri17@gmail.com>
2022-08-28 15:52:20 +0700

    feat: integration middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,5 +65,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
+        'checkUserSubscription' => \App\Http\Middleware\CheckUserSubscription::class,
+
     ];
 }
